5e77e5d4ee6353047517a56062c6221b2330d0e0
Osyrus85 <osyrus85@gmail.com>
2020-08-26 03:10:01 -0500

    trying hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Main from "./components/Landing/Main";
 import ContactPage from './components/Contact/Contactpage';
 import Portfolio from "./components/Portfolio/Portfolio";
@@ -7,11 +7,7 @@ import Portfolio from "./components/Portfolio/Portfolio";
 function App() {
   return (
     <Router>
-      <div>
-       
-        < Link to="/"/>
-       
-      </div>
+     
       <Route exact path="/" component={Main}/> 
       <Route path="/Portfolio" component={Portfolio}/>
       <Route  path="/Contact" component={ContactPage}/>
