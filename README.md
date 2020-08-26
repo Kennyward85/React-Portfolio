ddac7eb5b6a5ab505ab97a15de392b0d5e703de0
Osyrus85 <osyrus85@gmail.com>
2020-08-26 03:21:50 -0500

    Page works fine on locolhost reasearching methods of why it will not deploy properly with router to gitpages

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Updated portfolio with requested features using react
 
 
 ## Link
+https://github.com/Kennyward85/React-Portfolio
 
+https://kennyward85.github.io/React-Portfolio/
 
 ### Image
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Main from "./components/Landing/Main";
 import ContactPage from './components/Contact/Contactpage';
 import Portfolio from "./components/Portfolio/Portfolio";
