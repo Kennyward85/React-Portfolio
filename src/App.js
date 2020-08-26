7204feaebf0149b3aee5b10d63ee86f8ce8b1115
Osyrus85 <osyrus85@gmail.com>
2020-08-26 03:35:57 -0500

    Had to user HashRouter basename to get app to deploy on git properly Css has changed so modifying

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Main from "./components/Landing/Main";
 import ContactPage from './components/Contact/Contactpage';
 import Portfolio from "./components/Portfolio/Portfolio";
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename= "/">
      
       <Route exact path="/" component={Main}/> 
       <Route path="/Portfolio" component={Portfolio}/>
       <Route  path="/Contact" component={ContactPage}/>
 
-    </Router>  
+    </HashRouter>  
 );
 }
 
